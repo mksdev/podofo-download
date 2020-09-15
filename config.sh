7c464843ec6ee6be53aabf9472cd233b527947d2
Michal Němec <michal.nemec.dev@gmail.com>
2020-09-15 15:44:27 +0200

    Initial commit

---
+++ config.sh
@@ -0,0 +1,5 @@
+PROJECT_ROOT=$(pwd)/podofo
+AUTHORS_FILE=${PROJECT_ROOT}/authors.txt
+SVN_REPO=http://svn.code.sf.net/p/podofo/code/podofo/
+SVN_LAYOUT="--trunk=main --branches=branches --tags=tags"
+GIT_REPO=git@github.com:mksdev/podofo.git
